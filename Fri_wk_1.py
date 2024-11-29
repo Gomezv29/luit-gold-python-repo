85cb056aed4cb6c2680a81e0002a6c2acce76765
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-11-29 04:01:38 +0000

    notes

---
+++ Fri_wk_1.py
@@ -0,0 +1,13 @@
+# The While loop
+#while condition:
+ #   do_somthing()
+  #  do_somthing2()
+
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1 # do not forget to increment the variable in the while loop or else the value will always be 1 and just keep printing out 1
+print('Finished!')
+
+# Remember an infinite loop never ends (its condition is always true)
+# Remember Increasing by 1 is called incrementation.
