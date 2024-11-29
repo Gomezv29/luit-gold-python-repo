9f1065f89c74a09e23c6ec4d9e51dbd76734cc37
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-11-29 03:37:02 +0000

    exercise and solution

--- lec_30_exercise.py
+++ lec_30_exercise.py
@@ -1,8 +1,22 @@
-day = int(input('How many days ago have you purchased the item? '))
-usage = input('Have you used the item at all y/n? ')
-item = input('Has the item broken down on its own y/n? ')
+day_purchased = int(input('How many days ago have you purchased the item? '))
+usage_when = input('Have you used the item at all [y/n]? ')
+item_broken = input('Has the item broken down on its own [y/n]? ')
 
-if day < 10 and usage == 'y' and item == 'n' and item == 'y' or day > 10 and usage == 'n' and usage == 'y' and item == 'y':
-    print('You can get a refund')
+if item_broken == 'y' or day_purchased <= 10 and usage_when == 'n':
+    print('You can get a refund.')
 else:
-    print('You cannot get a refund')
+    print('You cannot get a refund.')
+    
+    
+    
+    
+    
+    # Solution
+purchase_days_ago = int(input('How many days ago have you purchased the item? '))
+is_used = input('Have you used the item at all [y/n]? ')
+is_broken = input('Has the item broken down on its own [y/n]? ')
+ 
+if(is_broken == 'y' or (purchase_days_ago <= 10 and is_used == 'n')):
+  print('You can get a refund.')
+else:
+  print('You cannot get a refund.')
