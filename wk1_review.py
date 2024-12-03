7da3a9ce7ab946ebafffa016c11855c6c4880ae6
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-03 22:52:28 +0000

    changes

--- wk1_review.py
+++ wk1_review.py
@@ -4,14 +4,14 @@ import random
 number = random.randint(0, 10)
 
 # Check if the number is less than 6
-#if number < 6:
- #   print('small number') # If true, print "small number"
+if number < 6:
+    print('small number') # If true, print "small number"
 # If the number is greater than 6
-#elif number > 6:
- #   print('big number') # If true, print "big number"
+elif number > 6:
+    print('big number') # If true, print "big number"
 # If neither of the above conditions is met, meaning the number is equal to 6
-#else:
- #   print('number is 6') # If true, print "number is 6"
+else:
+    print('number is 6') # If true, print "number is 6"
 
 
 # Check if the number is less than 4
