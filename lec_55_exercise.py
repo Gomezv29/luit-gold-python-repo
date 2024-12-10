4c21d58a8d1aa1ed134e0c6619a16f3f538576c0
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-10 06:41:41 +0000

    working through problem

---
+++ lec_55_exercise.py
@@ -0,0 +1,36 @@
+sample_dict = {
+    "mouth": "Mund",
+    "finger": "Finger",
+    "leg": "Bein",
+    "hand": "Hand",
+    "face": "Gesicht",
+    "nose": "Nase"
+}
+
+
+
+#while True:
+#    user_input = input('Enter a word in English or EXIT: ')
+ #   if sample_dict.keys():
+  #      print('Translation:', sample_dict.values()
+   # elif user_input == 'EXIT':
+    #    break
+    #else:
+     #   print('No match!')
+
+eng = sample_dict.keys()
+ger = sample_dict.values()
+     
+        
+        
+for trans in sample_dict.items():
+    user_input = input('Enter a word in English or EXIT: ')
+    if :
+        print('Translation:', ger)
+    elif user_input == 'EXIT':
+        break
+    else:
+        print('No match!')
+        
+        
+        
