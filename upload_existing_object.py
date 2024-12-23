38ea27996f30868d34d3d61cee0eba470b6ed86d
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-23 19:30:26 +0000

    add. changes

--- upload_existing_object.py
+++ upload_existing_object.py
@@ -2,5 +2,5 @@ import boto3
 
 s3 = boto3.client('s3')
 
-with open("testtext.txt", 'rb') as f:
-    s3.put_object(Bucket="vgomez-boto3-12222024", Key="testtext.txt", Body=f)
+with open("test_text.txt", 'rb') as f:
+    s3.put_object(Bucket="vgomez-boto3-12222024", Key="test_text.txt", Body=f)
